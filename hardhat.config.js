f31221b8539e7e2e5a6a0ef51cfc0a4ba6cde358
Suukkaa <suokkaa@gmail.com>
2021-11-23 09:58:25 +0900

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,6 +25,8 @@ module.exports = {
     },
   },
   etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
     apiKey: "9BC2IPE3VF84Z1AX9ZXYGEKSX6I1CUZQ1T",
   },
 };
